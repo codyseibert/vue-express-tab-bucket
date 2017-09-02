cec49f16427cdf5c652866290fc69d568c4e96a9
Cody Seibert <cseibert113@gmail.com>
2017-09-01 22:20:21 -0400

    changing position of elements

---
+++ client/src/components/viewSong/SongInfo.vue
@@ -0,0 +1,132 @@
+<template>
+  <div class="white elevation-2">
+    <v-toolbar flat dense class="cyan" dark>
+      <v-toolbar-title>Song Info</v-toolbar-title>
+    </v-toolbar>
+
+    <v-layout class="pb-2">
+      <v-flex xs8>
+        <v-layout column>
+          <v-flex>
+            <div class="text-xs-right p-4">
+              <div class="song-title">
+                <song-browser-link :value="song.title" />
+              </div>
+              <div class="song-artist">
+                <song-browser-link :value="song.artist" />
+              </div>
+              <div class="song-genre">
+                <song-browser-link :value="song.genre" />
+              </div>
+            </div>
+          </v-flex>
+
+          <v-flex v-if="$store.state.isUserLoggedIn">
+            <router-link
+              v-if="song.UserId === $store.state.user.id"
+              :to="{ name: 'editSong', params: { songId: this.song.id }}">
+              <v-btn dark primary class="cyan">
+                <v-icon>mode_edit</v-icon> Edit Song
+              </v-btn>
+            </router-link>
+
+            <v-btn v-if="bookmark" @click="unsetAsBookmark" dark primary class="cyan">
+              <v-icon>bookmark</v-icon> Unbookmark
+            </v-btn>
+
+            <v-btn v-if="!bookmark" @click="setAsBookmark" dark primary class="cyan">
+              <v-icon>bookmark_border</v-icon> Bookmark
+            </v-btn>
+          </v-flex>
+        </v-layout>
+      </v-flex>
+
+      <v-flex xs4>
+        <img class="album-image pt-2" :src="song.albumImage">
+        <p>
+          <song-browser-link :value="song.album" />
+        </p>
+      </v-flex>
+    </v-layout>
+  </div>
+</template>
+
+
+<script>
+import RecentsService from '@/services/RecentsService'
+import BookmarksService from '@/services/BookmarksService'
+import SongBrowserLink from '@/components/songs/SongBrowserLink'
+
+export default {
+  props: {
+    song: Object
+  },
+  components: {
+    SongBrowserLink
+  },
+  methods: {
+    async setAsBookmark () {
+      this.bookmark = await BookmarksService.post({
+        songId: this.song.id
+      })
+    },
+    async unsetAsBookmark () {
+      await BookmarksService.delete(this.bookmark.bookmarkId)
+      this.bookmark = null
+    }
+  },
+  async mounted () {
+    const songId = this.$store.state.route.params.songId
+    if (this.$store.state.isUserLoggedIn) {
+      this.bookmark = (await BookmarksService.index({
+        songId: songId
+      }))[0]
+      RecentsService.post({
+        songId: songId
+      })
+    }
+  }
+}
+</script>
+
+
+<style scoped>
+.song-title {
+  font-size: 30px;
+}
+
+.song-artist {
+  font-size: 24px;
+}
+
+.song-genre {
+  font-size: 18px;
+}
+
+.icon {
+  margin-right: 10px;
+}
+
+a {
+  text-decoration: none;
+}
+
+textarea {
+  font-family: monospace;
+  width: 100%;
+  border: none;
+  height: 400px;
+  border-style: none;
+  border-color: Transparent;
+  overflow: auto;
+  padding: 40px;
+}
+
+.p-4 {
+  padding: 20px;
+}
+
+.album-image {
+  width: 80%;
+}
+</style>
--- client/src/components/viewSong/ViewSong.vue
+++ client/src/components/viewSong/ViewSong.vue
@@ -1,138 +1,76 @@
 <template>
-  <v-layout class="song">
-    <v-flex xs6>
-      <div class="white elevation-2">
-        <v-toolbar flat dense class="cyan" dark>
-          <v-toolbar-title>Song Info</v-toolbar-title>
-        </v-toolbar>
+  <div>
+    <v-layout class="song">
+      <v-flex xs6>
+        <song-info :song="song" />
+      </v-flex>
 
-        <v-layout class="pb-2">
-          <v-flex xs8>
-            <v-layout column>
-              <v-flex>
-                <div class="text-xs-right p-4">
-                  <div class="song-title">
-                    <song-browser-link :value="song.title" />
-                  </div>
-                  <div class="song-artist">
-                    <song-browser-link :value="song.artist" />
-                  </div>
-                  <div class="song-genre">
-                    <song-browser-link :value="song.genre" />
-                  </div>
-                </div>
-              </v-flex>
+      <v-flex xs6>
+        <div class="white elevation-2">
+          <v-toolbar flat dense class="cyan" dark>
+            <v-toolbar-title>YouTube Video</v-toolbar-title>
+          </v-toolbar>
 
-              <v-flex v-if="$store.state.isUserLoggedIn">
-                <router-link
-                  v-if="song.UserId === $store.state.user.id"
-                  :to="{ name: 'editSong', params: { songId: this.song.id }}">
-                  <v-btn dark primary class="cyan">
-                    <v-icon>mode_edit</v-icon> Edit Song
-                  </v-btn>
-                </router-link>
-
-                <v-btn v-if="bookmark" @click="unsetAsBookmark" dark primary class="cyan">
-                  <v-icon>bookmark</v-icon> Unbookmark
-                </v-btn>
-
-                <v-btn v-if="!bookmark" @click="setAsBookmark" dark primary class="cyan">
-                  <v-icon>bookmark_border</v-icon> Bookmark
-                </v-btn>
-              </v-flex>
-            </v-layout>
-          </v-flex>
-
-          <v-flex xs4>
-            <img class="album-image pt-2" :src="song.albumImage">
-            <p>
-              <song-browser-link :value="song.album" />
-            </p>
-          </v-flex>
-        </v-layout>
-      </div>
-
-      <div class="white elevation-2 mt-2">
-        <v-toolbar flat dense class="cyan" dark>
-          <v-toolbar-title>YouTube Video</v-toolbar-title>
-        </v-toolbar>
-
-        <div class="p-4">
-          <youtube
-            :video-id="song.youtubeId"
-            :player-width="500"
-            :player-height="300">
-          </youtube>
+          <div class="p-4">
+            <youtube
+              :video-id="song.youtubeId"
+              :player-width="500"
+              :player-height="194">
+            </youtube>
+          </div>
         </div>
-      </div>
+      </v-flex>
+    </v-layout>
 
-    </v-flex>
+    <v-layout>
+      <v-flex xs6>
+        <div class="white elevation-2 mt-2">
+          <v-toolbar flat dense class="cyan" dark>
+            <v-toolbar-title>Tabs</v-toolbar-title>
+          </v-toolbar>
 
-    <v-flex xs6>
-      <div class="white elevation-2">
-        <v-toolbar flat dense class="cyan" dark>
-          <v-toolbar-title>Tabs</v-toolbar-title>
-        </v-toolbar>
+          <textarea
+            readonly
+            v-model="song.tab">
+          </textarea>
+        </div>
+      </v-flex>
 
-        <textarea
-          readonly
-          v-model="song.tab">
-        </textarea>
-      </div>
+      <v-flex>
+        <div class="white elevation-2 mt-2">
+          <v-toolbar flat dense class="cyan" dark>
+            <v-toolbar-title>Lyrics</v-toolbar-title>
+          </v-toolbar>
 
-      <div class="white elevation-2 mt-2">
-        <v-toolbar flat dense class="cyan" dark>
-          <v-toolbar-title>Lyrics</v-toolbar-title>
-        </v-toolbar>
-
-        <textarea
-          readonly
-          v-model="song.lyrics">
-        </textarea>
-      </div>
-    </v-flex>
-  </v-layout>
+          <textarea
+            readonly
+            v-model="song.lyrics">
+          </textarea>
+        </div>
+      </v-flex>
+    </v-layout>
+  </div>
 </template>
 
 
 <script>
 import SongsService from '@/services/SongsService'
-import RecentsService from '@/services/RecentsService'
-import BookmarksService from '@/services/BookmarksService'
 import SongBrowserLink from '@/components/songs/SongBrowserLink'
+import SongInfo from '@/components/viewSong/SongInfo'
 
 export default {
   data () {
     return {
-      song: {},
-      bookmark: null
+      song: {}
     }
   },
   components: {
-    SongBrowserLink
-  },
-  methods: {
-    async setAsBookmark () {
-      this.bookmark = await BookmarksService.post({
-        songId: this.song.id
-      })
-    },
-    async unsetAsBookmark () {
-      await BookmarksService.delete(this.bookmark.bookmarkId)
-      this.bookmark = null
-    }
+    SongBrowserLink,
+    SongInfo
   },
   async mounted () {
     const songId = this.$store.state.route.params.songId
     this.song = await SongsService.show(songId)
-    if (this.$store.state.isUserLoggedIn) {
-      this.bookmark = (await BookmarksService.index({
-        songId: songId
-      }))[0]
-      RecentsService.post({
-        songId: songId
-      })
-    }
   }
 }
 </script>
@@ -160,9 +98,10 @@ a {
 }
 
 textarea {
+  font-family: monospace;
   width: 100%;
   border: none;
-  height: 400px;
+  height: 600px;
   border-style: none;
   border-color: Transparent;
   overflow: auto;
